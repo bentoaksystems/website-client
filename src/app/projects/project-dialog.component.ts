d76bcec18256e6c618202c4df3e38c9ba4772037
arhariri <ali.71hariri@gmail.com>
2017-09-19 19:06:30 +0430

    Merge with master branch Using bootstrap instead flex on small and extra small screen in project and people pages Set direction on dialogs (people and projects pages) based on choosen language

--- src/app/projects/project-dialog.component.ts
+++ src/app/projects/project-dialog.component.ts
@@ -9,11 +9,14 @@ import {MD_DIALOG_DATA, MdDialogRef} from "@angular/material";
   styleUrls: ['./project-dialog.component.css']
 })
 export class ProjectDialogComponent implements OnInit {
+  lang: string
 
   constructor(public langService: LanguageService, public dialogRef: MdDialogRef<ProjectDialogComponent>,
               @Inject(MD_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
+    this.langService.lang$.subscribe(lang => this.lang = lang);
+
     this.data.width -= 24;
   }
 
