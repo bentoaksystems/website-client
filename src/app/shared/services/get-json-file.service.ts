b9ca2a8b0802d856c8755f8994e463567e9dec1d
305-2 <sareh@bentoak.systems>
2018-09-16 15:59:25 +0430

    add new service to read json files

--- src/app/shared/services/get-json-file.service.ts
+++ src/app/shared/services/get-json-file.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import {HttpService} from './http.service';
 
 @Injectable()
 export class GetJsonFileService {
+  headerData: any = {};
 
-  constructor() { }
+  constructor(private httpService: HttpService) {
+  }
+
+  getHeaderData() {
+    return new Promise((resolve, reject) => {
+      this.httpService.get('header').subscribe(
+        (data) => {
+          this.headerData = data;
+          resolve(data)
+        },
+        (err) => {
+          console.error('error');
+        }
+      );
+    });
+  }
+
 
 }
