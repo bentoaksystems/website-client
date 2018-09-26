4df8e121568617699afcddf91973c7abf797205c
305-2 <sareh@bentoak.systems>
2018-09-26 14:52:54 +0330

    fixing bug in project page responsive mode, remove some unused css classes

--- src/app/site/projects/project-dialog/project-dialog.component.ts
+++ src/app/site/projects/project-dialog/project-dialog.component.ts
@@ -1,7 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 
-import {LanguageService} from "../../../shared/services/language.service";
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 
 @Component({
   selector: 'app-project-dialog',
@@ -9,9 +8,8 @@ import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
   styleUrls: ['./project-dialog.component.css']
 })
 export class ProjectDialogComponent implements OnInit {
-  lang: string
 
-  constructor(public langService: LanguageService, public dialogRef: MatDialogRef<ProjectDialogComponent>,
+  constructor(public dialogRef: MatDialogRef<ProjectDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) {}
 
   ngOnInit() {
