115bfa10a84377e5e01396cb22fc8e9b2550c4d1
imans777 <iman.s_sani@yahoo.com>
2018-09-27 21:19:22 +0330

    apply theming to master

--- src/app/site/projects/project/project.component.ts
+++ src/app/site/projects/project/project.component.ts
@@ -1,9 +1,9 @@
 import {Component, Inject, Input, OnInit} from '@angular/core';
-import {MatDialog} from "@angular/material";
+import {MatDialog} from '@angular/material';
 
-import {LanguageService} from "../../../shared/services/language.service";
-import {WINDOW} from "../../../shared/services/window.service";
-import {ProjectDialogComponent} from "../project-dialog/project-dialog.component";
+import {LanguageService} from '../../../shared/services/language.service';
+import {WINDOW} from '../../../shared/services/window.service';
+import {ProjectDialogComponent} from '../project-dialog/project-dialog.component';
 
 @Component({
   selector: 'app-project',
@@ -12,10 +12,11 @@ import {ProjectDialogComponent} from "../project-dialog/project-dialog.component
 })
 export class ProjectComponent implements OnInit {
   @Input('project') project: any;
-  width: number = 500;
+  width = 500;
 
   constructor(public langService: LanguageService, @Inject(WINDOW) private window,
-    public dialog: MatDialog) {}
+              public dialog: MatDialog) {
+  }
 
   ngOnInit() {
     this.width = this.window.innerWidth;
@@ -29,12 +30,12 @@ export class ProjectComponent implements OnInit {
     const _height = this.window.innerHeight;
 
     this.dialog.open(ProjectDialogComponent, {
-      width: _width * 0.7 + 'px',
-      height: _height * 0.5 + 'px',
+      width: _width * 0.8 + 'px',
+      height: _height * 0.7 + 'px',
       data: {
         project: this.project,
-        width: _width * 0.7,
-        height: _height * 0.5
+        width: _width * 0.8,
+        height: _height * 0.7
       }
     })
   }
