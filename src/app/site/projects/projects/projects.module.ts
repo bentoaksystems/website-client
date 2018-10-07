c83741c1643293a2e03b941ab34e29e8f40ddcda
samanvaziri <saman.fv@gmail.com>
2018-10-07 14:42:34 +0330

    Merge branch 'master' of https://github.com/bentoaksystems/website-client into lazy-loading

---
+++ src/app/site/projects/projects/projects.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProjectsRoutingModule } from './projects-routing.module';
+@NgModule({
+  imports: [
+    CommonModule,
+    ProjectsRoutingModule,
+    // ProjectsComponent
+  ],
+  declarations: []
+})
+export class ProjectsModule { }
