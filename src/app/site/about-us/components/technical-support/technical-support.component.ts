678753e1021ed1645e6b1930e1b4e9a70295dc4e
Qazal <jalilian.gh@gmail.com>
2018-10-17 15:27:42 +0330

    merged whith another branch

--- src/app/site/about-us/components/technical-support/technical-support.component.ts
+++ src/app/site/about-us/components/technical-support/technical-support.component.ts
@@ -9,24 +9,6 @@ import * as marked from 'marked';
 })
 
 export class TechnicalSupportComponent implements OnInit {
-<<<<<<< HEAD
-     techsuppInfo;
-    desc;
-    isMobile = false;
-
-    @Input('techsuppInfo') set techsupp_Info (data) {
-        if(!data) return;
-        this.techsuppInfo = data.filter(x => x.title === 'Technical Support')[0];
-        this.desc = marked(this.techsuppInfo.description)
-      };
-
-    constructor(private responsiveService: ResponsiveService) { }
-
-    ngOnInit() {
-        this.isMobile = this.responsiveService.isMobile;
-        this.responsiveService.switch$.subscribe(isMobile => this.isMobile = isMobile);
-    }
-=======
   techsuppInfo;
   desc = null;
   isMobile = false;
@@ -38,7 +20,6 @@ export class TechnicalSupportComponent implements OnInit {
     this.techsuppInfo = data.filter(x => x.title === 'Technical Support')[0];
     this.desc = marked(this.techsuppInfo.description)
   };
->>>>>>> 97837ace3128778875ddbd47980d78d9c3fb3e58
 
   constructor(private responsiveService: ResponsiveService) {
   }
